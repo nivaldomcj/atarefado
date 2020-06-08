e1ba76454a58fd00b95b7a56200be6033f67b970
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-08 16:04:03 -0300

    Model for User Task

--- tasklist/models.py
+++ tasklist/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class TaskModel(models.Model):
+    """
+    An representation for a task made by a user.
+    """
+
+    class Meta:
+        verbose_name = "tarefa"
+        verbose_name_plural = "tarefas"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="usuário")
+    description = models.CharField(max_length=128, verbose_name="descrição")
+    schedule = models.DateTimeField(verbose_name="vencimento")
+    is_done = models.BooleanField(verbose_name="concluída")
+
+    def __str__(self):
+        return self.description
