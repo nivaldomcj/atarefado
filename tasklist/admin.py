35c8ad13c5579d86366cef0837a63f3f2ea2b9b9
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-08 16:41:37 -0300

    Added filters by date and user for TaskList Management

--- tasklist/admin.py
+++ tasklist/admin.py
@@ -3,7 +3,8 @@ from tasklist.models import TaskModel
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('user', 'description', 'schedule', 'is_done', 'is_overdue')
+    list_display = ('description', 'user', 'schedule', 'is_done', 'is_overdue')
+    list_filter = ['schedule', 'user']
 
 
 admin.site.register(TaskModel, TaskAdmin)
