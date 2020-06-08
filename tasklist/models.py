c9bc796b2fed6aed8bceac0ba34cc8826b2962fd
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-08 17:46:24 -0300

    Added model for daily review notification

--- tasklist/models.py
+++ tasklist/models.py
@@ -3,6 +3,25 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
+class NotificationModel(models.Model):
+    """
+    A daily review notification sent for a user.
+    """
+
+    class Meta:
+        db_table = "notification"
+        verbose_name = "notificação"
+        verbose_name_plural = "notificações"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="usuário")
+    text = models.CharField(max_length=255, verbose_name="texto")
+    sent_date = models.DateTimeField(verbose_name="data de envio")
+    was_sent = models.BooleanField(verbose_name="enviado")
+
+    def __str__(self):
+        return self.text
+
+
 class TaskModel(models.Model):
     """
     A representation for a task made by a user.
