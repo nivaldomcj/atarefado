c36083a0d52bd7b63368219d2908715afa6c7c15
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-11 16:02:29 -0300

    Set up `staticfiles`

--- atarefado/settings.py
+++ atarefado/settings.py
@@ -54,6 +54,7 @@ CRONJOBS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,5 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
