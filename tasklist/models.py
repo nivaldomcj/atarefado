06b67ae916449cd2d7f0df92736e488819c16e97
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-10 21:13:47 -0300

    Added `title` and `description` for Task

--- tasklist/models.py
+++ tasklist/models.py
@@ -33,7 +33,8 @@ class TaskModel(models.Model):
         verbose_name_plural = "tarefas"
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="usuário")
-    description = models.CharField(max_length=128, verbose_name="descrição")
+    title = models.CharField(max_length=128, verbose_name="título")
+    description = models.TextField(max_length=128, verbose_name="descrição")
     due_date = models.DateTimeField(verbose_name="vencimento")
     is_done = models.BooleanField(verbose_name="concluída")
 
