3635755916fe7c50e9742d5abb21961a174dd36a
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-11 09:20:00 -0300

    Notifications View only for current user

--- tasklist/views.py
+++ tasklist/views.py
@@ -2,10 +2,22 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from tasklist.serializers import UserSerializer
+from tasklist.serializers import (UserSerializer,
+                                  NotificationSerializer)
+from tasklist.models import NotificationModel
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+
+
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = NotificationSerializer
+
+    def get_queryset(self):
+        """
+        Only notifications for current user will be displayed
+        """
+        return NotificationModel.objects.filter(user=self.request.user)
