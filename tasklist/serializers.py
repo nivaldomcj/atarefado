dcbdab7be851765035923c7c08145d8b986f5202
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-11 09:19:31 -0300

    Serializer for Notifications

--- tasklist/serializers.py
+++ tasklist/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from tasklist.models import NotificationModel
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,3 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'is_superuser', 'is_staff',
                   'date_joined', 'last_login']
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationModel
+        fields = ['id', 'user', 'text', 'sent_date', 'was_sent']
