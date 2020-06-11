969dfe1642aa451fdf04b34832c5a7a994995b1b
Nivaldo Carvalho <nivaldo_j@outlook.com>
2020-06-11 09:20:52 -0300

    Included Notifications management for Admin Panel

--- tasklist/admin.py
+++ tasklist/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from tasklist.models import TaskModel
+from tasklist.models import NotificationModel
 
 
 class TaskAdmin(admin.ModelAdmin):
-    search_fields = ('description', 'user',)
-    list_display = ('description', 'user', 'due_date', 'is_done', 'is_overdue',)
-    list_filter = ('due_date', 'user',)
+    search_fields = ('description', 'user')
+    list_display = ('description', 'user', 'due_date', 'is_done', 'is_overdue')
+    list_filter = ('due_date', 'user')
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    search_fields = ('text', 'user')
+    list_display = ('text', 'user', 'sent_date', 'was_sent')
+    list_filter = ('sent_date', 'user', 'was_sent')
 
 
 admin.site.register(TaskModel, TaskAdmin)
+admin.site.register(NotificationModel, NotificationAdmin)
